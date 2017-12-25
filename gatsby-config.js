1e0d93d4eea50b5e03c2e665108dad1cea5a0225
orlo wang <ow.cc@outlook.com>
2017-12-25 17:01:06 +0800

    upgrade to react16

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-  },
-  plugins: [`gatsby-plugin-react-helmet`],
+    siteMetadata: {
+        title: `Gatsby Default Starter`,
+    },
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-styled-components`,
+        `gatsby-plugin-react-next`,
+    ],
 }
